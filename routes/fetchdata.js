92c4f4fb1652db76c921c6e3b46f643b31947d7f
AlaguAravindA <136082352+AlaguAravindA@users.noreply.github.com>
2024-02-13 07:35:12 +0530

    Update fetchdata.js

--- routes/fetchdata.js
+++ routes/fetchdata.js
@@ -3,31 +3,24 @@ const router = express.Router();
 const Watchlist = require('../models/watchlist');
 
 router.get('/:uid/:movieid', async (req, res) => {
-  
   try {
     const uid = req.params.uid;
     const movieId = req.params.movieid;
-   
-  
 
     // Find the watchlist for the specified user ID
     const userWatchlist = await Watchlist.findOne({ userId: uid });
-    
 
     if (!userWatchlist || userWatchlist.watchlist.length === 0) {
-     res.status(404).json({ error: 'Watchlist not found or empty' }).send("<h1 className='text-white'>No Watchlist found</h1>");
-
+      return res.status(404).send("<h1 className='text-white' >No Watchlist found</h1>");
     }
 
     // Check if the specified movie is in the watchlist
     const isInWatchlist = userWatchlist.watchlist.includes(movieId);
- 
 
     // Send the result in the response
     res.status(200).json({ isInWatchlist });
   } catch (error) {
-    
-    console.error('Error checking watchlist: in the movie', error.message);
+    console.error('Error checking watchlist in the movie:', error.message);
     res.status(500).json({ error: 'Internal server error' });
   }
 });
